2870fea16fb2f8cdf1ce84462a5d62a9fffaa8b9
Gustavo Menezes <verboty@gmail.com>
2023-04-28 13:36:34 -0300

    main

--- controller/validate.js
+++ controller/validate.js
@@ -13,27 +13,27 @@ const validate = {
     },
     notEmpty(args) {
         args.forEach(element => {
-            if(!element.trim().length) throw new Error(`o campo ${element} esta vazio`)
+            if(!element.trim().length) throw new Error(`o campo ${element} está vazio`)
         });
     },
     argumentsValidate(args) {
         args.forEach(({ keyName, value, type, notEmpty, optional }) => {
             if (value != undefined) {
                 if (type === 'array') {
-                    if (!Array.isArray(value)) throw new Error(`${keyName} ${value} no es tipo ${type}`);
+                    if (!Array.isArray(value)) throw new Error(`${keyName} ${value} não é tipo ${type}`);
                 }
                 if (typeof value !== type && type !== 'array') {
-                    if (keyName === "password") throw new Error(`la contraseña proporcionada no en tipo ${type}`)
-                    throw new Error(`${keyName} ${value} no es tipo ${type}`);
+                    if (keyName === "password") throw new Error(`a senha fornecida não está no tipo ${type}`)
+                    throw new Error(`${keyName} ${value} não é tipo ${type}`);
                 }
                 if (notEmpty) {
                     if (type === 'string') {
-                        if (!value.trim().length) throw new Error(`${keyName} está vacío`);
+                        if (!value.trim().length) throw new Error(`${keyName} está vazio`);
                     } else if (type === 'array') {
-                        if (value.length === 0) throw new Error(`${keyName} está vacío`);
+                        if (value.length === 0) throw new Error(`${keyName} está vazio`);
                     }
                 }
-            } else if (!optional) throw new Error(`${keyName} no es opcional`);
+            } else if (!optional) throw new Error(`${keyName} não é opcional`);
         })
     }
 
